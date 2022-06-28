e290084a22ed48d2bf7d5381fdabedb52c74f33f
Emmanuel Ekpele Nwasiwe <emmanuelekpelenwasiwe@gmail.com>
2022-06-28 05:01:13 -0700

    task1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,19 +2,25 @@
 import random
 number = random.randint(-10000, 10000)
 
-print("Last digit of {:d} is ".format(number), end="")
+# Verify if the number is positive and get the last digit
 
-if number < 0:
-last = number % -10
+if number >= 0:
 
-else:
-last = number % 10
+    lastdigit = number % 10
 
-if last > 5:
-    print("{:d} and is greater than 5".format(last))
-
-elif last == 0:
-    print("{:d} and is 0".format(last))
+# If the number is negative we multiply by a negative to have a positive number
 
 else:
-    print("{:d} and is less than 6 and not 0".format(last))
+
+    lastdigit = number % (-10)
+
+if lastdigit > 5:
+    print("Last digit of {} is {:d} and is greater than 5"
+          .format(number, lastdigit))
+
+if lastdigit == 0:
+    print("Last digit of {} is {:d} and is 0".format(number, lastdigit))
+
+elif lastdigit < 6 != 0:
+    print("Last digit of {} is {:d} and is less than 6 and not 0"
+          .format(number, lastdigit))
