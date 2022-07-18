8fa2697f67aca4ab50d18f22afab2be10fd83ea8
Emmanuel Ekpele Nwasiwe <emmanuelekpelenwasiwe@gmail.com>
2022-07-18 04:27:55 -0700

    101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_function(fct, *args):
+
+    try:
+        func = fct(*args)
+
+    except Exception as error:
+        func = None
+        sys.stderr.write("Exception: {}\n".format(error))
+
+    return (func)
