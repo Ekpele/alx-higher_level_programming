20f003554d8f70afd40300249b3bc919347a9f66
Emmanuel Ekpele Nwasiwe <emmanuelekpelenwasiwe@gmail.com>
2022-07-18 04:11:54 -0700

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,10 +4,8 @@ def safe_print_integer(value):
 
     try:
 
-    print("{:d}".format(value))
+        print("{:d}".format(value))
 
         return True
-
     except (TypeError, ValueError):
-
         return False
