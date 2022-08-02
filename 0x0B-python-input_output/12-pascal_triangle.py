ee620b7542ccd62f6f144532c7e5ee657123eed2
Emmanuel Ekpele Nwasiwe <emmanuelekpelenwasiwe@gmail.com>
2022-08-02 15:55:40 -0700

    12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-"""Module 14-pascal_triangle.
-Returns a list of lists of integers
-representing the Pascal’s triangle of n.
-"""
+'''
+file: 14-pascal_triangle.py
+functions:
+-> pascal_triangle
+'''
 
 
 def pascal_triangle(n):
-    """Returns the pascal triangle of n.
-    Args:
-        - n: size of the triangle (rows)
-    Returns: a list of list of integers
-    """
+    ''' Create a list of list representing pascal triangle '''
 
-    if n <= 0:
-        return []
+    triangle = []
+    if n == 0:
+        return triangle
 
-    l = [[0 for x in range(i + 1)] for i in range(n)]
-    l[0] = [1]
+    triangle.append([1])
 
     for i in range(1, n):
-        l[i][0] = 1
-        for j in range(1, i + 1):
-            if j < len(l[i - 1]):
-                l[i][j] = l[i - 1][j - 1] + l[i - 1][j]
-            else:
-                l[i][j] = l[i - 1][0]
-    return l
+        before = triangle[-1]
+        after = [1]
+        for i in range(len(before) - 1):
+            after.append(before[i] + before[i + 1])
+        after += [1]
+        triangle.append(after)
+
+    return triangle
