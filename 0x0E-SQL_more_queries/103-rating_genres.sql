8cbeb2a9fae3fe69fa990062f350733aefa7d2eb
Emmanuel Ekpele Nwasiwe <emmanuelekpelenwasiwe@gmail.com>
2022-09-08 01:39:26 -0700

    103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,18 +1,11 @@
--- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
--- run this first to import a SQL dump -->
---      echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
---      curl [link] -s | mysql -uroot -p hbtn_0d_tvshows
---
--- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
--- each record should display: tv_genres.name - number_shows
--- don’t display a genre that doesn’t have any shows linked
--- results must be sorted in descending order by the number of shows linked
--- can use only one SELECT statement
--- the database name will be passed as an argument of the mysql command
-
-    SELECT tv_genres.name AS genre, COUNT(*) AS 'number_shows'
-      FROM tv_genres
-INNER JOIN tv_show_genres
-        ON tv_genres.id = tv_show_genres.genre_id
-  GROUP BY genre
-  ORDER BY number_shows DESC;
+-- Script that lists genres by their rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating -- Query to join tables
+FROM tv_genres
+JOIN tv_show_genres
+     ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows
+     ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_show_ratings
+     ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
